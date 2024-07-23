832896b4a19d28cab7a0f89fd7e1e0600eaebde1
saigirishpabbathi <saigirishpabbathi.com@gmail.com>
2024-07-23 11:34:10 -0400

    Role Service Added

--- src/main/java/com/RestroRealm/App/Repository/UserRepository.java
+++ src/main/java/com/RestroRealm/App/Repository/UserRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
     List<User> findAllUsersByFirstNameOrLastName(String firstName, String lastName);
 
