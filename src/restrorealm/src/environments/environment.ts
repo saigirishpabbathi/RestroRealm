10e8cbe43b1cbe82d9bad00ba7c95a8f8dee3e83
sravan123-project <bodakondasravankumar@gmail.com>
2025-03-15 10:20:34 -0400

    new Updates

--- src/restrorealm/src/environments/environment.ts
+++ src/restrorealm/src/environments/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
     production: false,
     apiUrl: 'http://localhost:8080/api/v1',
     imageUrl: 'http://localhost:8080',
-    stripeKey: 'pk_test_51Qml33GzzbXfbtkhTJnPn6NpyTMKIYtH8wRYqwNVUWPqQLqjkP7sEQVRBTAF6WYHQdJbUg9ssdZ0WJFgGdNZEg0Y0079CDCSlL',
+    stripeKey: 'pk_test_51QxX4UK840hddnWYszVWwwBF7D3AAk8NC0hi6qcqrH2Keioq9QY8FEhzAnXWELeRBUfO7Pspe1pkLR0KJMg3Eo8u00MZKCYO1i',
     paypalClientId : 'AThoF9xCES1-XFFMaRPUorYplBSErSNiF9TD2v-04wirrjT0Okh_bPPrGUO-GwtiwAf6gto5sk4A0LMl'
   };
